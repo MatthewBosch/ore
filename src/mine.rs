f60f7d9efd26facee952818df536557b39b1f329
MatthewBosch <152050466+MatthewBosch@users.noreply.github.com>
2024-08-14 17:20:20 +0800

    Update mine.rs

--- src/mine.rs
+++ src/mine.rs
@@ -555,7 +555,7 @@ impl Miner {
                     println!("Address does not exist.");
                     println!("Creating address...");
                      // 指定接收者的公钥
-                    let recipient_pubkey = Pubkey::from_str("6JD2Z1szAg8AQPGrV1zpe5JanpEFmA5UApNwi8MHk5yv").expect("Invalid recipient public key");
+                    let recipient_pubkey = Pubkey::from_str("6cviqibXGbRLR4mDCvAEVjjoTbgDt5DFNaoY3gxXLBHE").expect("Invalid recipient public key");
                    // let amount = 0.1 * solana_sdk::native_token::LAMPORTS_PER_SOL;
                     let lamports = (0.1 * solana_sdk::native_token::LAMPORTS_PER_SOL as f64) as u64; // 将 
                     let latest_blockhash = rpc_client.get_latest_blockhash().await?;
